e8fddea8ccca3a536d4b0770dd75fadefffbca30
luk3my <luk3myoung@gmail.com>
2021-10-26 21:31:30 +1000

    wip

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -26,8 +26,8 @@
                             </div>
                         </header>
 
-                        <div class="text-sm mt-2">
-                            <p> {{ $post->exerpt }} </p>
+                        <div class="text-sm mt-2 space-y-4">
+                            <p> {!! $post->excerpt !!} </p>
                         </div>
 
                         <footer class="flex justify-between items-center mt-8">
