25d5b60395a7ed8dfe795f3973889e9b95188d99
luk3my <luk3myoung@gmail.com>
2021-11-02 20:00:14 +1000

    wip

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,15 @@
+<article class="flex bg-gray-100 p-6">
+    <div class="flex-shrink-0 mr-8">
+        <img src="https://i.pravatar.cc/60" alt="" />
+    </div>
+    <div>
+        <header>
+            <h3 class="font-bold">Some User</h3>
+            <p class="text-xs">Posted <time>8 months ago</time></p>
+        </header>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    </div>
+</article>
